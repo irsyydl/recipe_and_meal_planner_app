c1973729d0ac3115393ad873b8bac2c7abaeb022
Muh. Hafiz <93205535+Hafizz7@users.noreply.github.com>
2023-11-20 18:17:47 +0800

    auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      print('Error signing in: $e');
+      return null;
+    }
+  }
+
+  Future<User?> registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      print('Error registering: $e');
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  Future<String?> getCurrentUserId() async {
+    try {
+      User? user = _auth.currentUser;
+      return user?.uid;
+    } catch (e) {
+      print('Error getting current user ID: $e');
+      return null;
+    }
+  }
+}
