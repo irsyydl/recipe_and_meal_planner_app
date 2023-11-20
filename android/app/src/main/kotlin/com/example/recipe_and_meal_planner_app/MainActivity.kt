57809da7e10f5eab5f0eee42928c9f37655985f7
Irsyadul Asyrof <irsyadulasyrof@gmail.com>
2023-11-20 17:45:46 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/recipe_and_meal_planner_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipe_and_meal_planner_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:recipe_and_meal_planner_app/firebase_options.dart';
+import 'package:recipe_and_meal_planner_app/screens/authentication/login_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/authentication/registration_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/home_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/meal_planner_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/recipe_upload_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/shopping_list_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipe and Meal Planner',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        '/home': (context) => HomeScreen(),
+        '/shoppingList': (context) => ShoppingListScreen(),
+        '/mealPlanner': (context) => MealPlannerScreen(),
+        '/uploadRecipe': (context) => RecipeUploadScreen(),
+      },
+    );
+  }
+}
