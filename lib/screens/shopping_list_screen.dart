11ca3827134c3a6d74066973227e9572dfc92af9
Muh. Hafiz <93205535+Hafizz7@users.noreply.github.com>
2023-12-05 18:18:26 +0800

    Update Shopping List Screen Juga

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_and_meal_planner_app/services/shopping_list_service.dart';
 
-class ShoppingListScreen extends StatelessWidget {
+class ShoppingListScreen extends StatefulWidget {
+  @override
+  State<ShoppingListScreen> createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends State<ShoppingListScreen> {
   final ShoppingListService _shoppingListService = ShoppingListService();
 
   @override
@@ -10,39 +15,31 @@ class ShoppingListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Shopping List'),
       ),
-      body: FutureBuilder<List<String>>(
+      body: FutureBuilder<List<Map<String, String>>>(
         future: _shoppingListService.getShoppingList(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            List<String> shoppingList = snapshot.data ?? [];
-            return ListView.builder(
-              itemCount: shoppingList.length,
-              itemBuilder: (context, index) {
-                return ShoppingListItem(
-                  itemName: shoppingList[index],
-                );
-              },
-            );
-          }
-        },
-      ),
+    List<Map<String, String>> shoppingList = snapshot.data ?? [];
+
+    return ListView.builder(
+      itemCount: shoppingList.length,
+      itemBuilder: (context, index) {
+        String ingredient = shoppingList[index]['ingredient']!;
+        String recipeId = shoppingList[index]['recipeId']!;
+
+        return ListTile(
+          title: Text(ingredient),
+          trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () async {
+              await _shoppingListService.removeIngredientFromShoppingList(recipeId, ingredient);
+              setState(() {});
+            },
+          ),
+        );
+      },
     );
-  }
-}
-
-class ShoppingListItem extends StatelessWidget {
-  final String itemName;
-
-  const ShoppingListItem({Key? key, required this.itemName}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(itemName),
+  },
+),
     );
   }
 }
