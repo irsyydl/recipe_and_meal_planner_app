eb752776a5abccffa92144554204d69f9092d288
Rezkyns <rezkysya7@gmail.com>
2023-12-05 11:11:10 +0800

    Update meal planner screen
    Add an option to delete recipe from meal planner

--- lib/screens/meal_planner_screen.dart
+++ lib/screens/meal_planner_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_and_meal_planner_app/services/meal_planner_service.dart';
+import 'package:recipe_and_meal_planner_app/services/recipe_service.dart';
 
-class MealPlannerScreen extends StatelessWidget {
+class MealPlannerScreen extends StatefulWidget {
+  @override
+  State<MealPlannerScreen> createState() => _MealPlannerScreenState();
+}
+
+class _MealPlannerScreenState extends State<MealPlannerScreen> {
   final MealPlannerService _mealPlannerService = MealPlannerService();
+  final RecipeService _recipeService = RecipeService();
 
   @override
   Widget build(BuildContext context) {
@@ -28,26 +35,42 @@ class MealPlannerScreen extends StatelessWidget {
                 String date = mealPlanner.keys.elementAt(index);
                 List<Map<String, dynamic>> recipes = mealPlanner[date]!;
 
-                return ListTile(
-                  title: Text(date),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      for (var recipe in recipes)
-                        FutureBuilder<String>(
-                          future: _mealPlannerService.getRecipeTitle(recipe['recipeId']),
-                          builder: (context, titleSnapshot) {
-                            if (titleSnapshot.connectionState == ConnectionState.waiting) {
-                              return CircularProgressIndicator();
-                            } else if (titleSnapshot.hasError) {
-                              return Text('Error fetching recipe title');
-                            } else {
-                              return Text(titleSnapshot.data ?? 'Unknown Recipe');
-                            }
-                          },
-                        ),
-                    ],
+                return ExpansionTile(
+                  title: Text(
+                    date,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  children: recipes.map((recipe) {
+                    return FutureBuilder<String>(
+                      future: _recipeService
+                          .getRecipeTitle(recipe['recipeId']),
+                      builder: (context, titleSnapshot) {
+                        if (titleSnapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return CircularProgressIndicator();
+                        } else if (titleSnapshot.hasError) {
+                          return Text('Error fetching recipe title');
+                        } else {
+                          return GestureDetector(
+                            child: ListTile(
+                              title: Text(titleSnapshot.data ?? 'Unknown Recipe'),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () async {
+                                  await _mealPlannerService.removeFromMealPlanner(
+                                      recipe['recipeId'], date);
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    );
+                  }).toList(),
                 );
               },
             );
