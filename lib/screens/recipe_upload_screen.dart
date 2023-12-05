66abbac2a9376474d264df8012dde28e7cce1d5b
FitraNoviarji <113648587+FitraNoviarji@users.noreply.github.com>
2023-12-05 11:27:12 +0800

    Update recipe upload screen
    Improve ui and add option to take a photo for the recipe image.

--- lib/screens/recipe_upload_screen.dart
+++ lib/screens/recipe_upload_screen.dart
@@ -5,7 +5,10 @@ import 'package:image_picker/image_picker.dart';
 import 'package:recipe_and_meal_planner_app/services/recipe_service.dart';
 
 class RecipeUploadScreen extends StatefulWidget {
+  const RecipeUploadScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _RecipeUploadScreenState createState() => _RecipeUploadScreenState();
 }
 
@@ -21,46 +24,58 @@ class _RecipeUploadScreenState extends State<RecipeUploadScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Upload Recipe'),
+        title: const Text('Upload Recipe'),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              _image != null ? Image.file(_image!) : SizedBox(height: 16),
+              GestureDetector(
+                onTap: () => _showPickImageDialog(context),
+                child: Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: _image != null
+                      ? Image.file(_image!, fit: BoxFit.cover)
+                      : Icon(Icons.add_a_photo, color: Colors.grey[800]),
+                ),
+              ),
               TextFormField(
                 controller: titleController,
-                decoration: InputDecoration(labelText: 'Title'),
+                decoration: const InputDecoration(labelText: 'Title'),
               ),
               TextFormField(
                 controller: descriptionController,
-                decoration: InputDecoration(labelText: 'Description'),
+                decoration: const InputDecoration(labelText: 'Description'),
               ),
               TextFormField(
                 controller: ingredientsController,
-                decoration: InputDecoration(labelText: 'Ingredients'),
+                decoration: const InputDecoration(labelText: 'Ingredients'),
               ),
               TextFormField(
                 controller: instructionsController,
-                decoration: InputDecoration(labelText: 'Instructions'),
+                decoration: const InputDecoration(labelText: 'Instructions'),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  await _pickImage();
-                },
-                child: Text('Pick Image'),
-              ),
+              const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () async {
                   String title = titleController.text;
                   String description = descriptionController.text;
-                  List<String> ingredients =
-                      ingredientsController.text.split(',');
-                  List<String> instructions =
-                      instructionsController.text.split(',');
+                  List<String> ingredients = ingredientsController.text.split(',');
+                  List<String> instructions = instructionsController.text.split(',');
+
+                  if (title.isEmpty || description.isEmpty || ingredientsController.text.isEmpty || instructionsController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please fill all the fields before uploading the recipe.'))
+                    );
+                    return;
+                  }
 
                   await _uploadRecipe(
                     title,
@@ -78,10 +93,62 @@ class _RecipeUploadScreenState extends State<RecipeUploadScreen> {
     );
   }
 
-  Future<void> _pickImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+  void _showPickImageDialog(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (builder) {
+        return Container(
+          height: 150.0,
+          color: Colors.transparent,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+            ),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Pick from gallery'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      _pickImage();
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.camera_alt),
+                    title: const Text('Take a photo'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      _takePhoto();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 
+  Future _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future _takePhoto() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
       setState(() {
         _image = File(pickedFile.path);
@@ -103,12 +170,16 @@ class _RecipeUploadScreenState extends State<RecipeUploadScreen> {
         instructions: instructions,
         imageFile: _image,
       );
-
-      print('Recipe uploaded successfully!');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Recipe Uploaded!.'),
+        ),
+      );
+      Navigator.pop(context);
     } catch (e) {
       print('Error uploading recipe: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
+        const SnackBar(
           content: Text('Error uploading recipe. Please try again.'),
         ),
       );
