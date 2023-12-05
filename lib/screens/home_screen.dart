74b042256759d9581b8eb339bddac13f9957436c
Rezkyns <rezkysya7@gmail.com>
2023-12-05 11:21:11 +0800

    Update home screen
    Change FutureBuilder to StreamBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,49 +3,23 @@ import 'package:recipe_and_meal_planner_app/models/recipe.dart';
 import 'package:recipe_and_meal_planner_app/screens/recipe_details_screen.dart';
 // import 'package:recipe_and_meal_planner_app/services/auth_service.dart';
 import 'package:recipe_and_meal_planner_app/services/recipe_service.dart';
+import 'package:recipe_and_meal_planner_app/widgets/app_drawer.dart';
 import 'package:recipe_and_meal_planner_app/widgets/recipe_card.dart';
 
 class HomeScreen extends StatelessWidget {
   final RecipeService _recipeService = RecipeService();
-  // final AuthService _authService = AuthService();
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Recipes'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.person),
-            onPressed: () {
-              Navigator.pushNamed(context, '/profile');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              // Navigate to Shopping List screen
-              Navigator.pushNamed(context, '/shoppingList');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.calendar_today),
-            onPressed: () {
-              // Navigate to Meal Planner screen
-              Navigator.pushNamed(context, '/mealPlanner');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.upload),
-            onPressed: () {
-              // Navigate to Recipe Upload screen
-              Navigator.pushNamed(context, '/uploadRecipe');
-            },
-          ),
-        ],
+        title: Text('Home Page'),
       ),
-      body: FutureBuilder<List<Recipe>>(
-        future: _recipeService.getRecipes(),
+      drawer: AppDrawer(),
+      body: StreamBuilder<List<Recipe>>(
+        stream: _recipeService.getRecipes(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -77,9 +51,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
-  // Future<void> _logout(BuildContext context) async {
-  //   await _authService.signOut();
-  //   Navigator.pushReplacementNamed(context, '/login');
-  // }
 }
