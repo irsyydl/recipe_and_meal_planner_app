e4914a1bb1c0c9ad13eb498ddfa69f82784cfcce
Rezkyns <rezkysya7@gmail.com>
2023-12-05 11:16:24 +0800

    Update recipe card
    Add container to show the recipe's image in recipe card

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:recipe_and_meal_planner_app/models/recipe.dart';
 import 'package:recipe_and_meal_planner_app/services/meal_planner_service.dart';
-// import 'package:recipe_and_meal_planner_app/services/shopping_list_service.dart';
 
 class RecipeCard extends StatelessWidget {
   final Recipe recipe;
   final VoidCallback onTap;
-  // final ShoppingListService _shoppingListService = ShoppingListService();
   final MealPlannerService _mealPlannerService = MealPlannerService();
 
   RecipeCard({required this.recipe, required this.onTap});
@@ -16,36 +15,29 @@ class RecipeCard extends StatelessWidget {
     return Card(
       elevation: 3,
       margin: EdgeInsets.all(8),
-      child: ListTile(
-        title: Text(recipe.title),
-        subtitle: Text(recipe.description),
+      child: GestureDetector(
         onTap: onTap,
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // IconButton(
-            //   icon: Icon(Icons.add_shopping_cart),
-            //   onPressed: () async {
-            //     DateTime selectedDate = DateTime.now();
-            //     _mealPlannerService.addToMealPlanner(recipe.id, selectedDate);
-
-            //     await _shoppingListService.addToShoppingList(recipe.id);
-
-            //     ScaffoldMessenger.of(context).showSnackBar(
-            //       SnackBar(
-            //         content:
-            //             Text('Recipe added to meal planner and shopping list.'),
-            //       ),
-            //     );
-            //   },
-            // ),
-            IconButton(
-              icon: Icon(Icons.event),
-              onPressed: () {
-                _showDatePicker(context, recipe);
-              },
-            ),
-          ],
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Image.network(recipe.imageUrl, fit: BoxFit.cover),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(recipe.title, style: TextStyle(fontSize: 16)),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.event),
+                    onPressed: () {
+                      _showDatePicker(context, recipe);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -60,13 +52,10 @@ class RecipeCard extends StatelessWidget {
     );
 
     if (selectedDate != null) {
-      TimeOfDay? selectedDateTime =
-          await showTimePicker(context: context, initialTime: TimeOfDay.now());
-
       _mealPlannerService.addToMealPlanner(recipe.id, selectedDate);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Recipe added to meal planner on $selectedDateTime'),
+          content: Text('Recipe added to meal planner on ${DateFormat('yyyy-MM-dd').format(selectedDate)}'),
         ),
       );
     }
