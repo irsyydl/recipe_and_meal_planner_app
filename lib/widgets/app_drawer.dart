01369c57ae530484726265f3ade92ed3b3ef6f61
Irsyadul Asyrof <irsyadulasyrof@gmail.com>
2023-12-07 07:03:12 +0800

    Add logo to app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -4,54 +4,51 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Navigation Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Image.asset(
+                'assets/dishdash-high-resolution-logo.png',
+                fit: BoxFit.contain,
               ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Profile'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/profile');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Shopping List'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/shoppingList');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.calendar_today),
-            title: Text('Meal Planner'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/mealPlanner');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.upload),
-            title: Text('Upload Recipe'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/uploadRecipe');
-            },
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/profile');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text('Shopping List'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/shoppingList');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.calendar_today),
+              title: Text('Meal Planner'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/mealPlanner');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.upload),
+              title: Text('Upload Recipe'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/uploadRecipe');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
