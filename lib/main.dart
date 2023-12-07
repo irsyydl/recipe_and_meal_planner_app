c448e285c81fb926834ad1a4eabbbfb726ad7604
Irsyadul Asyrof <rezkysya7@gmail.com>
2023-12-07 13:21:53 +0800

    Update main.dart
    Change initial route to intro screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:recipe_and_meal_planner_app/firebase_options.dart';
 import 'package:recipe_and_meal_planner_app/screens/authentication/login_screen.dart';
 import 'package:recipe_and_meal_planner_app/screens/authentication/registration_screen.dart';
 import 'package:recipe_and_meal_planner_app/screens/home_screen.dart';
+import 'package:recipe_and_meal_planner_app/screens/introduction_screen.dart';
 import 'package:recipe_and_meal_planner_app/screens/meal_planner_screen.dart';
 import 'package:recipe_and_meal_planner_app/screens/profile_screen.dart';
 import 'package:recipe_and_meal_planner_app/screens/recipe_upload_screen.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/login',
+      initialRoute: '/intro',
       routes: {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegistrationScreen(),
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
         '/mealPlanner': (context) => MealPlannerScreen(),
         '/uploadRecipe': (context) => const RecipeUploadScreen(),
         '/profile': (context) => const ProfileScreen(),
+        '/intro': (context) => IntroductionScreen(),
       },
     );
   }
